058b31f460e863e5b0e912f455ca3d094c7b13bc
qaddo taha <musta.tt2015@gmail.com>
2025-06-19 22:49:00 +0300

    -- make  Extensions for the  project services  to make the program cs clean

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using OrphanSystem.Extensions;
+
+namespace VotingSystem.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services)
+        {
+            services.AddAuthConfig();
+            services.AddAutoMapperConfig();
+            services.AddSignalRConfig();
+            services.AddSwaggerConfig();
+
+            services.AddServices(); // your own service registrations
+            services.AddHttpContextAccessor();
+
+            services.AddCustomRouting();
+            services.AddCustomControllers();
+            services.AddEndpointsApiExplorer();
+
+            services.AddValidatorsFromAssemblyContaining<Program>();
+            services.AddFluentValidationAutoValidation();
+            services.AddMemoryCache();
+
+            services.AddTmpStuff();
+            services.AddControllersWithViews();
+            services.AddLocalizationConfig();
+
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,14 @@
 using FluentValidation.AspNetCore;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+
 using OrphanSystem.Data.Interceptors;
 using OrphanSystem.Data;
 using OrphanSystem.Extensions;
 using OrphanSystem.Helpers;
-
-
-#region Setup
-#if SET_LEGACY_TIMESTAMP
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-#endif
-/*
-#if USE_FIREBASE
-FirebaseApp.Create(new AppOptions()
-{
-    Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configs/Firebase/ServiceAccount.json"))
-});
-#endif*/
-#endregion
- 
+using VotingSystem.Extensions;
 
 #region App Builder
-
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigProvider.config = builder.Configuration;
@@ -33,50 +19,9 @@ builder.Services.AddDbContext<MasterDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
 });
 
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("AllowSpecificOrigins",
-        builder => builder
-            .WithOrigins(
-                "http://localhost:6969", "http://127.0.0.1:6969", "http://100.42.190.178:6969",
 
-                "http://localhost:3000", "http://127.0.0.1:3000", "http://100.42.190.178:3000",
-                "http://localhost:3001", "http://127.0.0.1:3001", "http://100.42.190.178:3001",
-                "http://localhost:3002", "http://127.0.0.1:3002", "http://100.42.190.178:3002",
-
-                "http://localhost:8000", "http://127.0.0.1:8000", "http://100.42.190.178:8000",
-                "http://localhost:8001", "http://127.0.0.1:8001", "http://100.42.190.178:8001",
-                "http://localhost:8002", "http://127.0.0.1:8002", "http://100.42.190.178:8002",
-
-                "http://localhost:8080", "http://127.0.0.1:8080", "http://100.42.190.178:8080",
-                "http://localhost:8081", "http://127.0.0.1:8081", "http://100.42.190.178:8081",
-                "http://localhost:8082", "http://127.0.0.1:8082", "http://100.42.190.178:8082"
-                )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials()
-            .WithExposedHeaders("status", "Content-Disposition", "Access-Control-Allow-Origin")
-    );
-});
-
-builder.Services.AddAuthConfig();
-builder.Services.AddAutoMapperConfig();
-builder.Services.AddSignalRConfig();
-builder.Services.AddSwaggerConfig();
-
-builder.Services.AddServices();
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddCustomRouting();
-builder.Services.AddCustomControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-builder.Services.AddFluentValidationAutoValidation();
-
-builder.Services.AddTmpStuff();
-builder.Services.AddControllersWithViews();
-builder.Services.AddLocalizationConfig();
+// if  u want to edit the project services u find  it in the Extensions Folder /ServiceCollectionExtensions class 
+builder.Services.AddProjectServices();
 
 #endregion
 
@@ -87,17 +32,12 @@ var app = builder.Build();
 app.UseCustomSwagger();
 app.UseHttpsRedirection();
 app.UseHsts();
-app.UseCors("AllowSpecificOrigins");
+app.UseCors("AllowAllOriginsWithLimitedMethods");
 app.UseRouting();
 app.UseErrResponseExceptionHandler();
 app.UseAuth();
 app.UseStaticFiles();
-/*
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapHub<MasterHub>("/hubs/master");
-    endpoints.MapControllers();
-});*/
+
 
 // Seeder
 
@@ -105,16 +45,13 @@ using (var scope = app.Services.CreateScope())
 {
     var dataContext = scope.ServiceProvider.GetRequiredService<MasterDbContext>();
     var seeder = new Seeder(dataContext);
-    
-    await seeder.SeedSuperAdmin("Abdelqader@Abdelqader.com");
+
+    await seeder.SeedSuperAdmin("SuperAdmin@SuperAdmin.com");
     if (app.Environment.IsDevelopment())
     {
         bool ignoreExistingRecords = false;
-        //await seeder.SeedCountriesCities(ignoreExistingRecords);
     }
 }
-
-
 
 app.MapControllerRoute(
     name: "default",
